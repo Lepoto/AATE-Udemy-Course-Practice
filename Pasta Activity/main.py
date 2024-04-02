f18697d14e36b03ba85091bf496a298f1490f049
percymasekwamen <33199000-percymasekwamen@users.noreply.replit.com>
2024-04-02 15:18:41 +0000

    Initial commit

---
+++ Pasta Activity/main.py
@@ -0,0 +1,19 @@
+import time
+
+name = input("What is your name? ")
+print("hello, " + name + " " + ", Instructions on how to cook Pasta")
+
+time.sleep(2)
+print("Boil some water")
+time.sleep(2)
+print("Wait for 5 minutes")
+time.sleep(2)
+print("Add Salt")
+time.sleep(2)
+print("Add Pasta")
+time.sleep(2)
+print("Cook for 10 minutes")
+time.sleep(2)
+print("Remove the water")
+time.sleep(2)
+print("Done!")
---
+++ Sport Activity/main.py
@@ -0,0 +1,9 @@
+import time
+
+name = input("What is your name? ")
+print("Hello,  " + name)
+time.sleep(1)
+sport = input("What is your favorite sport? ")
+time.sleep(2)
+
+print("Your favorite sport is " + sport)
